6f134b21140b25a392f69fe02865b9b4451af909
tanjil-hosain <tanjilh85@gmail.com>
2026-05-02 19:49:55 +0600

    database connection

--- config/db.php
+++ config/db.php
@@ -11,3 +11,10 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
