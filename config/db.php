cbf66bc303b15b0f8871112bd724b27f5d8b9620
tanjil-hosain <tanjilh85@gmail.com>
2026-05-02 19:48:58 +0600

    db

--- config/db.php
+++ config/db.php
@@ -4,3 +4,10 @@ $db   = 'bazaar_mama';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
