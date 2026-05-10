69927a0ae5c7abec554c8a9a6f4d7b8a65c54505
tanjil-hosain <tanjilh85@gmail.com>
2026-05-10 21:16:36 +0600

    header

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
