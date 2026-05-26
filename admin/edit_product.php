048546ab8763fba216412a3c46e975b046e745b4
tanjil-hosain <tanjilh85@gmail.com>
2026-05-26 22:04:12 +0600

    edit_product

--- admin/edit_product.php
+++ admin/edit_product.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8"><title>BazaarMama - Edit Product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<body>
+<body class="bg-light py-5">
+    <div class="container" style="max-width: 850px;">
+    </div>
     
 </body>
 </html>
