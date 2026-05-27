892caa10f9b05dfa7822ce2b26fc863e5b0612a2
tanjil-hosain <tanjilh85@gmail.com>
2026-05-27 22:24:12 +0600

    manag_product

---
+++ admin/manage_products.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8"><title>Available Products - BazaarMama</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
